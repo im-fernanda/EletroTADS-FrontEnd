16087b90a83013d74e354b76c37fb4869cce772a
Fernanda <gilneidefernandaf@gmail.com>
2024-08-24 20:05:39 -0300

    rotas arrumadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,12 @@ import { Routes } from '@angular/router';
 import { appConfig } from './app.config';
 
 export const routes: Routes = [
-  
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/enderecos-list/enderecos-list.component').then((c) => c.EnderecosListComponent),
+  },
+
   {
     path: 'enderecos', loadComponent: () => import('./components/enderecos-list/enderecos-list.component')
       .then(
@@ -15,5 +20,6 @@ export const routes: Routes = [
     path: 'criar-enderecos', loadComponent: () => import('./components/create-enderecos/create-enderecos.component').then(
       (c) => c.CreateEnderecosComponent,
     ),
-  }
+  },
+  { path: '**', redirectTo: 'enderecos' },
 ];
