215faac696e9c943409560351cf55619e33dbaad
Paulo-Abrantes <pauloaugusto123123@gmail.com>
2024-08-24 19:28:46 -0300

    arrumando enderecos-lists

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,20 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-address',
-  standalone: true,
-  imports: [],
   templateUrl: './address.component.html',
-  styleUrl: './address.component.scss'
+  styleUrls: ['./address.component.css']
 })
 export class AddressComponent {
-  @Input() rua: string = '';
-  @Input() numero: string = '';
-  @Input() bairro: string = '';
-  @Input() complemento: string = '';
-  @Input() cidade: string = '';
-  @Input() uf: string = '';
 
+  @Input() enderecosList: Address[] = []; // Lista de endereços que será exibida na tabela
+
+  // Métodos de edição e exclusão
+  edit(endereco: Address): void {
+    // Implementar lógica de edição
+  }
+
+  deleteById(endereco: Address): void {
+    // Implementar lógica de exclusão
+  }
 }
+
+// Definição da interface Address, que pode estar em um arquivo separado, por exemplo, address.model.ts
+export interface Address {
+  id: number;
+  rua: string;
+  numero: string;
+  bairro: string;
+  complemento: string;
+  cidade: string;
+  uf: string;
+}
+
 
