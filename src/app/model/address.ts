887a237ca4f87fb6c86e945a773536bb31be45cc
Fernanda Felix <118316479+im-fernanda@users.noreply.github.com>
2024-08-26 22:21:06 -0300

    Update address.ts

--- src/app/model/address.ts
+++ src/app/model/address.ts
@@ -1,13 +1,13 @@
 export class Address {
 
   constructor(
-    public id: number, // Identificador único do endereço
-    public rua: string, // Nome da rua
-    public numero: string, // Número da residência
-    public bairro: string, // Bairro
-    public complemento: string, // Complemento do endereço (ex: apartamento)
-    public cidade: string, // Nome da cidade
-    public uf: string // Sigla do estado (ex: RN para Rio Grande do Norte)
+    public id: number, 
+    public rua: string, 
+    public numero: string, 
+    public bairro: string, 
+    public complemento: string, 
+    public cidade: string, 
+    public uf: string 
   ) {}
 
 
