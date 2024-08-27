c408608905a133770a63727fa8666a056221eb03
Fernanda Felix <118316479+im-fernanda@users.noreply.github.com>
2024-08-27 12:59:23 -0300

    Update address.component.ts

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { Address } from '../../model/address'; // Importa a classe Address (ajuste o caminho conforme necessário)
+import { Address } from '../../model/address'; 
 
 @Component({
-  selector: 'app-address', // Nome da tag HTML usada para inserir este componente
-  standalone: true, // Indica que o componente pode ser usado independentemente
-  templateUrl: './address.component.html', // Caminho para o template HTML do componente
+  selector: 'app-address', 
+  standalone: true,
+  templateUrl: './address.component.html', 
 })
 export class AddressComponent {
 
-  // Propriedade de entrada (input) que recebe uma lista de endereços para exibição
-  @Input() enderecosList: Address[] = []; // Lista de endereços a ser exibida na tabela
+  // Propriedade de entrada que recebe uma lista de endereços para exibição
+  @Input() enderecosList: Address[] = []; 
 
 }
 
